5ad64960cc8fce966d5ce4a20fa1211392228843
Hwayeon <ggomi0011@gmail.com>
2024-12-08 18:40:33 +0900

    Update

--- dataset/augmentation.py
+++ dataset/augmentation.py
@@ -1,4 +1,3 @@
-import torch
 import random
 import numpy as np
 
@@ -8,7 +7,10 @@ class SignalAugmentation(object):
         self.x = x
         self.y = y
         self.gn_scaling = np.arange(0.05, 0.1, 0.01)  # for 5% ~ 10% gaussian noise.
-        self.n_permutation = 4
+        self.n_permutation = 5
+        self.second = 10
+        self.sampling_rate = 500
+        self.input_length = self.second * self.sampling_rate
 
     def random_gaussian_noise(self, p=0.5):
         """Randomly add Gaussian noise to all channels."""
@@ -36,28 +38,36 @@ class SignalAugmentation(object):
         aug_x = []
 
         # 1. Segment
-        # for: 각각의 input x (sample) 에 대해 노이즈를 추가할지 말지 결정
-        # 조건: random.random() < p 이면 순열 적용
-        # indexes = input_length 범위 안에서, n_permutation -1 개의 인덱스를 무작위로 선택.
-        # indexes = 정렬된 상태로 변환
-        # indexes = 리스트의 시작과 끝에 0과 input_length (데이터 조각의 길이)를 추가
+        for sample in x:
+            if random.random() < p:
+                indexes = list(np.random.choice(self.input_length, self.n_permutation - 1, replace=False))
+                indexes += [0, self.input_length]
+                indexes = list(np.sort(indexes))
 
-        # for문 활용, 해당 indexes에 맞게 데이터를 segment -> samples 리스트에 segment들 저장
+                # for문 활용, 해당 indexes에 맞게 데이터를 segment -> samples 리스트에 segment들 저장
+                segments = []
+                for idx_1, idx_2 in zip(indexes[:-1], indexes[1:]):
+                    segments.append(sample[:, idx_1:idx_2])
 
-        # 2. Shuffle
-        # 데이터 조각의 인덱스를 np.random.permutation
-        # 순열된 인덱스에 따라 samples에서 데이터 조각 선택 -> nx에 추가
+                # 2. Shuffle
+                # 데이터 조각의 인덱스를 np.random.permutation
+                shuffled_segments = []
+                for idx in np.random.permutation(np.arange(self.n_permutation)):
+                    # 순열된 인덱스에 따라 samples에서 데이터 조각 선택 -> shuffled_segments에 추가
+                    shuffled_segments.append(segments[idx])
 
-        # 3. Merge
-        # np.concatenate 를 활용해 전체 합치기 (axis=-1)
+                # 3. Merge
+                # np.concatenate 를 활용해 전체 합치기 (axis=-1)
+                shuffled_segments = np.concatenate(shuffled_segments, axis=-1)
+                aug_x.append(shuffled_segments)
 
         # else:  random.random() > p 에 해당한다면 순열 적용하지 않고, 바로 데이터 추가
-        # aug_x.append(sample)
-
+        else:
+            aug_x.append(shuffled_segments)
 
         # 데이터 반환
-        # aug_x = np.array(aug_x)  # np.array 형태로 바꿔서 출력
-        # return aug_x
+        aug_x = np.array(aug_x)  # np.array 형태로 바꿔서 출력
+        return aug_x
 
 
 
@@ -65,4 +75,4 @@ class SignalAugmentation(object):
 
 
 
-        return self.x[..., self.n_permutation, :], self.y
+
